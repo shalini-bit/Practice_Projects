d94763130bcd4a7066f6d22e61218bfa4eaabe2b
shalini-bit <shalinis0205@gmail.com>
2024-03-24 00:11:07 +0530

    fourth commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu_btn");
+const navLinks = document.getElementById("nav_links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
@@ -15,27 +15,29 @@ navLinks.addEventListener("click", (e) => {
 });
 
 const scrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 1000,
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
 };
 
-ScrollReveal.reveal(".header_image img" , {
-    ...scrollRevealOption,
-    origin: "right",
+ScrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header_content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header_content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
 });
 
-ScrollReveal.reveal(".header_image h2" , {
-    ...scrollRevealOption,
-    delay:500,
+ScrollReveal().reveal(".order_card", {
+  ...scrollRevealOption,
+  interval: 500,
 });
 
-ScrollReveal.reveal(".header_image h1" , {
-    ...scrollRevealOption,
-    delay: 1000,
-});
-
-ScrollReveal().reveal(".order_card" , {
- ...scrollRevealOption,
- interval:500,
+ScrollReveal().reveal(".event_content", {
+  duration: 1000,
 });
