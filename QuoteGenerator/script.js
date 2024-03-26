d9e5752e35a2e5ace09fe3de6df1df44694c80ee
shalini-bit <shalinis0205@gmail.com>
2024-03-26 12:42:53 +0530

    third commit

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -1,13 +1,16 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
+const URL = "https://api.quotable.io/random";
 const newquote = document.querySelector("#quote");
+const newauthor = document.querySelector("#author");
 const btn = document.querySelector("#btn");
+
 
 
 
 const getQuotes = async () => {
     let response = await fetch (URL);
     let data = await response.json();
-    newquote.innerText = data[0].text;
+    newquote.innerText = data.content;
+    newauthor.innerText = "-" + data.author;
 }
 
 btn.addEventListener("click" ,getQuotes);
